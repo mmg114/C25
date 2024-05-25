0ae6bfb4bcb11aa6462234006d75172fdf73a167
Mauricio Munoz Guzman <mauricio.m@moabits.com>
2024-05-24 19:28:19 -0500

    first commit

---
+++ src/main/java/Clase2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Clase2 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite su Nombre");
+        String nombre=scanner.nextLine();
+
+        System.out.println("Buen dia, su nombre es " +nombre);
+
+        System.out.println("Digite su edad");
+        int edad= scanner.nextInt();
+        System.out.println("Su edad es " +edad);
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/MiPrimeraClase.java
@@ -0,0 +1,45 @@
+
+
+//PODEMOS ESCRIBIR
+
+//TODO falta terminar
+
+/*
+Multi Linea
+*/
+
+import java.util.Scanner;
+
+public class MiPrimeraClase {
+
+
+    public static void main(String[] args) {
+        //primitivos
+        long numeroLargos;
+        int numero1;
+        double fraccion1;
+        boolean esUnNumero;
+        char letra='A';
+
+
+        //wrapper
+        Integer numero2;
+        Double fraccion2;
+        Boolean bolean;
+        Character caracter2;
+        Long numerosLargos2;
+        //Objetos
+        String palabra="Hola mundo";
+
+        int a;
+        int b =25;
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1;
+        scanner1= new Scanner(System.in);
+        System.out.println("Hola mundo.");
+
+
+
+
+    }
+}
